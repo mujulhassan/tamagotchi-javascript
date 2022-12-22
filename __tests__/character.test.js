876c9e5bacd897056eee3b08c36d4807c6bb28a2
mujulhassan <mujulhassan@gmail.com>
2022-12-22 20:07:07 +0000

    assigning character name

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -4,3 +4,12 @@ describe("constructor", () => {
       expect(new Character("Mametchi")).toBeInstanceOf(Object);
     });
   });
+
+describe("constructor", () => {
+    // ...
+  it("sets the name property", () => {
+    const character = new Character("Mametchi");
+
+    expect(character.name).toEqual("Mametchi");
+  });
+});
--- src/character.js
+++ src/character.js
@@ -1,3 +1,5 @@
-function Character() {} // create the Character function
+function Character(name) {
+    this.name = name;
+} // create the Character function
  
  module.exports = Character; // export it from the file
